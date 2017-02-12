fdab779bd838483b4bfae702cf36da677ee2dd47
wansong <betterofsong@gmail.com>
2017-02-12 22:26:31 +0800

    Initial commit

---
+++ Example/ZKVideoCache/ZKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZKAppDelegate.h
+//  ZKVideoCache
+//
+//  Created by bestofsong on 02/12/2017.
+//  Copyright (c) 2017 bestofsong. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ZKVideoCache/ZKViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZKViewController.h
+//  ZKVideoCache
+//
+//  Created by bestofsong on 02/12/2017.
+//  Copyright (c) 2017 bestofsong. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZKViewController : UIViewController
+
+@end
